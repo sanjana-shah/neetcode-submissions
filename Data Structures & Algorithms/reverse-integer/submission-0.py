fc161db1aaa1f94cf7fc8e0633919acaba7fdd7e
Sanjana Shah <53399985+sanjana-shah@users.noreply.github.com>
2026-08-15 14:01:42 -0400

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        mi, mx = -2**31, 2**31 - 1
+
+        reverse = 0
+
+        # -1234
+        if x < 0:
+            while x < 0:
+                if reverse < (mi/10):
+                    return 0
+
+                reverse *= 10
+                temp = (x * -1) % 10
+                print(temp)
+                reverse -= temp
+                print(reverse)
+                print(x)
+                x = int(x/10)
+                print(x)
+
+        else:
+            while x > 0:
+                if reverse > (mx/10):
+                    return 0
+
+                reverse *= 10
+                reverse += (x % 10)
+                x = x // 10
+
+        return reverse
+
+
+        
